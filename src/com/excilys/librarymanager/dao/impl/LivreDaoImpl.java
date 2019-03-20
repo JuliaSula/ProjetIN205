b18528d81ea8c32d57dee8fadfa217457f9f16e3
Julia <julia.sula@hotmail.com>
2019-03-20 15:45:49 -0300

    Projet IN205

--- src/com/excilys/librarymanager/dao/impl/LivreDaoImpl.java
+++ src/com/excilys/librarymanager/dao/impl/LivreDaoImpl.java
@@ -9,50 +9,114 @@ import java.util.List;
 
 import com.excilys.librarymanager.dao.LivreDao;
 import com.excilys.librarymanager.exception.DaoException;
-import com.excilys.librarymanager.modele.Emprunt;
 import com.excilys.librarymanager.modele.Livre;
-import com.excilys.librarymanager.modele.Membre;
 import com.excilys.librarymanager.persistence.ConnectionManager;
 
 public class LivreDaoImpl implements LivreDao{
-
+	
+	/*Implementation Singleton*/
+	private static LivreDaoImpl instance;
+	private LivreDaoImpl() { }
+	public static LivreDaoImpl getInstance() {
+		if(instance == null) {
+			instance = new LivreDaoImpl();
+		}
+		return instance;
+	}
+	
+	private static final String GET_LIVRES_QUERY = "SELECT * FROM livre;";
+	private static final String GET_BY_ID_QUERY = "SELECT id, titre, auteur, isbn FROM livre WHERE id = ?;";
+	private static final String UPDATE_QUERY = "UPDATE livre SET titre = ?, auteur = ?, isbn = ? WHERE id = ?;";
+	private static final String DELETE_QUERY = "DELETE FROM livre WHERE id = ?;";
+	private static final String COUNT_QUERY = "SELECT COUNT(id) AS count FROM livre;";
 	@Override
-	public List<Livre> getList() throws DaoException, SQLException {
-		ArrayList<Livre> livreList = new ArrayList<Livre>();
-		 Connection connection = ConnectionManager.getConnection();
-	     PreparedStatement selectPreparedStatement = null;
-	     String SelectQuery = "SELECT * FROM livre";
-	     try {
-	    	 connection.setAutoCommit(false);
-	    	 selectPreparedStatement = connection.prepareStatement(SelectQuery);
-	         ResultSet rs = selectPreparedStatement.executeQuery();
-	       
+	public List<Livre> getList() throws DaoException {
+		ArrayList<Livre> livreList = new ArrayList<>();
+		/*Objets de connexion*/
+		ResultSet rs=null;
+		Connection connection=null;
+		PreparedStatement preparedStatement=null;
+	    /*Bloc de try */
+		try {
+	    	 connection = ConnectionManager.getConnection();
+		     //String SelectQuery = "SELECT * FROM livre";
+		     preparedStatement = connection.prepareStatement(GET_LIVRES_QUERY);
+	    	 
+//Pq il met pas le autocommit de le execution
+		     
+	         rs = preparedStatement.executeQuery();
+	         /*Prende des valeurs tant qu`il y a et met dans la liste*/
 	         while (rs.next()) {
-	        	 /*FAULT COMPLETER*/
 	         Livre livre= new Livre(rs.getString("titre"), rs.getString("auteur"), rs.getString("isbn"));
-	         
 	         livreList.add(livre);
-	         
 	         }
-	         selectPreparedStatement.close();
-	         connection.commit();
-	         //return film;
-	     } catch (SQLException e) {
-	         System.out.println("Exception Message " + e.getLocalizedMessage());
-	         //throw DaoException
-	     } catch (Exception e) {
-	         e.printStackTrace();
-	     } finally {
-	         connection.close();
-	     }
+		} 
+		catch (SQLException e) {
+			throw new DaoException("Problème lors de la récupération de la liste des livres", e);
+		}finally {
+			try{
+				rs.close();
+			}catch(Exception e){
+				e.printStackTrace();
+			}try {
+				preparedStatement.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			try {
+				connection.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		} 
+	   //return film;
 		return livreList;
 	}
 
 	@Override
 	public Livre getById(int id) throws DaoException {
-		// TODO Auto-generated method stub
-		return null;
-	}
+		/*Constructeur livre*/
+		Livre livre = new Livre();
+		/*Objets de connexion*/
+		ResultSet rs=null;
+		Connection connection=null;
+		PreparedStatement preparedStatement=null;
+		try {
+			/*Connexion*/
+			connection = ConnectionManager.getConnection();
+			preparedStatement = connection.prepareStatement(GET_BY_ID_QUERY);
+			preparedStatement.setInt(1, id);
+			rs = preparedStatement.executeQuery();
+			/*Set des valeurs de objets par query*/
+			while (rs.next()) {
+				livre.setIdLivre(rs.getInt("id"));
+				livre.setTitre(rs.getString("titre"));
+				livre.setIsbn(rs.getString("isbn"));
+				livre.setAuteur(rs.getString("auteur"));
+	         }
+			
+			}catch (SQLException e) {
+	 			throw new DaoException("Problème lors de la récupération du livre: id=" + id, e);
+		}finally {
+		
+			try{
+				rs.close();
+			}catch(Exception e){
+				e.printStackTrace();
+			}try {
+				preparedStatement.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			try {
+				connection.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+			return livre;
+	} 
+	
 
 	@Override
 	public int create(String titre, String auteur, String isbn) throws DaoException {
@@ -62,20 +126,101 @@ public class LivreDaoImpl implements LivreDao{
 
 	@Override
 	public void update(Livre livre) throws DaoException {
-		// TODO Auto-generated method stub
+		//ResultSet rs=null;
+		Connection connection=null;
+		PreparedStatement preparedStatement=null;
+		try {
+			/*Connexion*/
+			connection = ConnectionManager.getConnection();
+			preparedStatement = connection.prepareStatement(UPDATE_QUERY);
+			preparedStatement.setString(1, livre.getTitre());
+			preparedStatement.setString(2, livre.getAuteur());
+			preparedStatement.setString(3, livre.getIsbn());
+			preparedStatement.executeUpdate();
+			
+			System.out.println("UPDATE: " + livre);
+		}
+	catch (SQLException e) {
+		throw new DaoException("Problème lors de la mise à jour du luivre: " + livre, e);
+	} finally {
 		
+		try {
+			preparedStatement.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		try {
+			connection.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 	}
 
 	@Override
 	public void delete(int id) throws DaoException {
-		// TODO Auto-generated method stub
+		Connection connection=null;
+		PreparedStatement preparedStatement=null;
+		try {
+			/*Connexion*/
+			connection = ConnectionManager.getConnection();
+			preparedStatement = connection.prepareStatement(DELETE_QUERY);
+			preparedStatement.setInt(1, id);
+			preparedStatement.executeUpdate();
+			preparedStatement.close();
+			connection.close();
+			System.out.println("DELETE: " + id);
+			}catch (SQLException e) {
+				throw new DaoException("Problème lors de la suppression du film: "+id, e);
+			}  finally {
+				try {
+					preparedStatement.close();
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+				try {
+					connection.close();
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
 		
 	}
 
 	@Override
 	public int count() throws DaoException {
-		// TODO Auto-generated method stub
-		return 0;
+		int count;
+		ResultSet rs=null;
+		Connection connection=null;
+		PreparedStatement preparedStatement=null;
+		try {
+			connection = ConnectionManager.getConnection();
+			preparedStatement = connection.prepareStatement(COUNT_QUERY);
+			rs = preparedStatement.executeQuery();
+			count=rs.getInt("count");
+			System.out.println("UPDATE: " + count);
+		}
+		catch (SQLException e) {
+			throw new DaoException("Problème lors compteur le film: " , e);
+		}
+	 finally {
+		try {
+			rs.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		try {
+			preparedStatement.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		try {
+			connection.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
 	}
-
-}
+		return count;
+	}
+	}
--- src/com/excilys/librarymanager/exception/DaoException.java
+++ src/com/excilys/librarymanager/exception/DaoException.java
@@ -2,6 +2,15 @@ package com.excilys.librarymanager.exception;
 
 public class DaoException extends Exception{
 
-	DaoException()
-	{}
+	public DaoException() {
+		super();
+	}
+
+	public DaoException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	public DaoException(String message) {
+		super(message);
+	}
 }
